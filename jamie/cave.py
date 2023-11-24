600f0acd8ede85d21122cefc0dd5d097e3b0bbd5
JamieIsMe <120991704+JamieIsMe@users.noreply.github.com>
2023-11-24 22:49:25 +0000

    xamp

--- jamie/cave.py
+++ jamie/cave.py
@@ -8,13 +8,19 @@ class Cave(Location):
         self.current_location = "entrance"
 
     def entrance(self):
-        print("As you cautiously step into the dimly lit cave, two things stand out to you, iron bars with a door "
+        print("As you cautiously step into the dimly lit cave, two things stand out to you, iron bars with a dw"
               "blocking further access into the cave and drawings on the cave walls")
         while self.current_location == "entrance":
-            # Should Use List Of Possible Actions And Input With Numbers? or vague Ask What The user wants to do and
-            # let them type in keywords like "door" "bars" or "wall" "painting"
-            self.current_location = "cavern"
 
+            # EXAMPLE: LIST OF ACTIONS
+            action = input("What Will You Do?\n1) Turn back around\n2) Go to the iron door\n3) Check the walls\n")
+            if action == "1":
+                print("As you attempt to make your way back out of the cave, you trip on a rock, somehow causing the "
+                      "entrance to cave in")
+            elif action == "2":
+                print("its made of iron, what did you expect?")
+            elif action == "3":
+                print("wow this wall is made of wall")
 
 if __name__ == "__main__":
     cave = Cave()
