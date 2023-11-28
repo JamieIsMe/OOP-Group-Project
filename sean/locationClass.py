2b64b5a81902d04236cb6a81fe1f0efcca43e8f3
Sean <merville05@gmail.com>
2023-11-28 00:53:54 +0000

    Updated Location Class to have a visited locations method

--- sean/locationClass.py
+++ sean/locationClass.py
@@ -1,9 +1,10 @@
 class Location:
-    def __init__(self, location, sublocation, location_npcs, clues):
+    def __init__(self, location, sublocation, location_npcs, clues, visited):
         self.location = location
         self.sublocation = sublocation
         self.location_NPCs = location_npcs
         self.__clues = clues
+        self.__visited_sublocations = visited
 
     def add_clue(self, clue):
         self.__clues.append(clue)
@@ -19,6 +20,12 @@ class Location:
 
     def specific_location(self):
         return self.sublocation
+
+    def visited(self, location):
+        self.__visited_sublocations.append(location)
+
+    def view_visited_locations(self):
+        return f"You have visited {self.__visited_sublocations}"
 
     def whoishere(self):
         return self.location_NPCs
