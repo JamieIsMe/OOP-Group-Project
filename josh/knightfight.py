8fd3a1b83f78a66d9b43c275c25f1717a19cc268
Joshua Sunil Mathew <119957696+joshuasunil10@users.noreply.github.com>
2023-11-30 13:51:17 +0000

    Create knightfight.py

---
+++ josh/knightfight.py
@@ -0,0 +1,6 @@
+def knightfight():
+    print("========================")
+    print("Final Challenge - Duel with the Knight")
+    print("There will be a number generated randomly, from 1 to 3")
+    print("if the player - guesses the correct number, that amount of damage is done to the knight")
+    print("if the player does not get the correct answer, the player loses health, depending on their answer")
